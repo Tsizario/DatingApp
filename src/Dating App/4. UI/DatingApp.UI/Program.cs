d634161ee47eda5d4a4e19aa95806a7a592d0002
Tsizario <oleksandr.tsisarenko@nure.ua>
2022-11-09 01:04:19 +0200

    Updated Program.cs in WebApi

--- src/Dating App/4. UI/DatingApp.UI/Program.cs
+++ src/Dating App/4. UI/DatingApp.UI/Program.cs
@@ -1,25 +1,52 @@
-var builder = WebApplication.CreateBuilder(args);
+using DatingApp.DAL.ApplicationSeed;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace API
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var startup = new Startup(builder.Configuration);
+
+            startup.ConfigureServices(builder.Services);
+
+            var app = builder.Build();
+
+            startup.Configure(app);
+
+            if (args.Length == 1 && args[0].ToLower() == "/seed")
+            {
+                RunSeeding(app);
+            }
+            else
+            {
+                app.Run();
+            }
+        }
+
+        private static void RunSeeding(WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+                try
+                {
+                    var seeder = scope.ServiceProvider.GetRequiredService<AppUserSeed>();
+                    seeder.SeedUsersAsync().Wait();
+                    logger.LogInformation("The seeding is done");                    
+                }
+                catch(Exception ex)
+                {
+                    logger.LogError(ex, "An error occured duting seeding");
+                }
+            }
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
