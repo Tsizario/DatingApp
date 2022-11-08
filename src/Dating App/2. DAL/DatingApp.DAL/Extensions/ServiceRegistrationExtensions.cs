a71a6babf5d99e804ed5465af58094e1f28ebff1
Tsizario <oleksandr.tsisarenko@nure.ua>
2022-11-09 01:01:53 +0200

    Added ServiceRegistrationExtensions.cs to DAL

---
+++ src/Dating App/2. DAL/DatingApp.DAL/Extensions/ServiceRegistrationExtensions.cs
@@ -0,0 +1,27 @@
+﻿using DatingApp.DAL.ApplicationSeed;
+using DatingApp.DAL.Repositories.UserRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DatingApp.DAL.Extensions
+{
+    public static class ServiceRegistrationExtensions
+    {
+        public static IServiceCollection AddDataAccessLayerServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<AppContext>(config =>
+            {
+                config.UseSqlServer(
+                    configuration.GetConnectionString("CourseDatingAppConnection"));
+            });
+
+            services.AddScoped<AppUserSeed>();
+
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            return services;
+        }
+    }
+}
