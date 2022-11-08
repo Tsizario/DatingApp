187333533a8af651f964cf00c0a9de89331d3ba7
Tsizario <oleksandr.tsisarenko@nure.ua>
2022-11-09 00:52:11 +0200

    Added minimal Startup.cs

---
+++ src/Dating App/4. UI/DatingApp.UI/Startup.cs
@@ -0,0 +1,41 @@
+using DatingApp.DAL.Extensions;
+
+namespace API
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDataAccessLayerServices(_configuration);
+
+            services.AddCors();       
+        }
+
+        public void Configure(WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
