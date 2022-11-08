41e99bae486de927d554742b5dfe8c740f95b2c0
Tsizario <oleksandr.tsisarenko@nure.ua>
2022-11-09 00:33:24 +0200

    Renamed UserName to Username property

--- src/Dating App/1. Domain/DatingApp.Domain/Entities/AppUser.cs
+++ src/Dating App/1. Domain/DatingApp.Domain/Entities/AppUser.cs
@@ -6,7 +6,7 @@ namespace DatingApp.Domain.Entities
     {
         public int Id { get; set; }
 
-        public string UserName { get; set; }
+        public string Username { get; set; }
 
         public string FirstName { get; set; }
 
