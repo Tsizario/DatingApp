0eedca00af561a714ccb23542fff7ff0d7ced093
Tsizario <oleksandr.tsisarenko@nure.ua>
2022-11-09 00:36:14 +0200

    Added minimal AppContext.cs

---
+++ src/Dating App/2. DAL/DatingApp.DAL/AppContext.cs
@@ -0,0 +1,22 @@
+﻿using DatingApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.DAL
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<AppUser>()
+                .Property(i => i.Id)
+                .ValueGeneratedOnAdd();
+        }
+    }
+}
