1dff292cfd7c4bcf4fbdd831d3d9fbb92a3a3f9a
Tsizario <oleksandr.tsisarenko@nure.ua>
2022-11-09 00:35:28 +0200

    Added minimal functionality for AppUser

---
+++ src/Dating App/2. DAL/DatingApp.DAL/Repositories/UserRepository/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using DatingApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.DAL.Repositories.UserRepository
+{
+    internal class UserRepository : IUserRepository
+    {
+        private readonly AppContext _dbContext;
+
+        public UserRepository(AppContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<AppUser> GetUserByIdAsync(int userId)
+        {
+            return await _dbContext.Users.FindAsync(userId);
+        }
+
+        public async Task<AppUser> GetUserByUsernameAsync(string username)
+        {
+            return await _dbContext.Users
+                .SingleOrDefaultAsync(user => user.UserName == username);
+        }
+
+        public async Task<IEnumerable<AppUser>> GetAllUsersAsync()
+        {
+            return await _dbContext.Users.ToListAsync();
+        }
+
+        public async Task<AppUser> AddUserAsync(AppUser appUser)
+        {
+            await _dbContext.Users.AddAsync(appUser);
+
+            await _dbContext.SaveChangesAsync();
+
+            return appUser;
+        }
+
+        public async Task<bool> ExistsAsync(string username)
+        {
+            return await _dbcontext.Users.AnyAsync(x => x.Username == username.ToLower());
+        }       
+    }
+}
