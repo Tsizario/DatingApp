5ecbf2e4365d7d755c76ac6e1f763163a1f3a285
Tsizario <oleksandr.tsisarenko@nure.ua>
2022-11-09 00:15:56 +0200

    Added IUserRepository: first version for getting data

---
+++ src/Dating App/2. DAL/DatingApp.DAL/Repositories/UserRepository/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using DatingApp.Domain.Entities;
+
+namespace DatingApp.DAL.Repositories.UserRepository
+{
+    public interface IUserRepository
+    {
+        Task<AppUser> GetUserByIdAsync(int id);
+
+        Task<AppUser> GetUserByUsernameAsync(string username);
+
+        Task<IEnumerable<AppUser>> GetAllUsersAsync();
+
+        Task<AppUser> AddUserAsync(AppUser addedEntity);
+
+        Task<bool> ExistsAsync(string username);
+    }
+}
