0894b6f0cf27eef303ec485d0b7aef9916914a03
Tsizario <oleksandr.tsisarenko@nure.ua>
2022-11-09 00:45:48 +0200

    Added seeder for database

---
+++ src/Dating App/2. DAL/DatingApp.DAL/ApplicationSeed/AppUserSeed.cs
@@ -0,0 +1,48 @@
+﻿using DatingApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+
+namespace DatingApp.DAL.ApplicationSeed
+{
+    public class AppUserSeed
+    {
+        private readonly AppContext _context;
+
+        public AppUserSeed(AppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedUsersAsync()
+        {
+            _context.Database.EnsureCreated();
+
+            if (await _context.Users.AnyAsync())
+                return;
+
+            var filePath = new DirectoryInfo(
+                    Path.Combine(Environment.CurrentDirectory,
+                        @"..\..\2. DAL\DAL\ApplicationSeed"))
+                            .GetFiles("*.json")
+                                .FirstOrDefault()?.FullName;
+
+            var userData = await File.ReadAllTextAsync(filePath);
+            var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+
+            foreach (var user in users)
+            {
+                using var hmac = new HMACSHA256();
+
+                user.Username = user.Username.ToLower();
+                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("password"));
+                user.PasswordSalt = hmac.Key;
+
+                _context.Users.Add(user);
+            }
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
