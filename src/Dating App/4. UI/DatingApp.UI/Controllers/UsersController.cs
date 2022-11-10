e5545ca7cb28b95b369a0b71c1560d36c12af7d4
Tsizario <oleksandr.tsisarenko@nure.ua>
2022-11-10 02:55:38 +0200

    Added UsersController with minimal functionality

---
+++ src/Dating App/4. UI/DatingApp.UI/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using DatingApp.BLL.DTO;
+using DatingApp.BLL.Services.UserService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.WebApi.Controllers
+{
+    [Route("[controller]/")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUserDto>>> GetUsers()
+        {
+            var users = await _userService.GetAllAppUsers();
+
+            if (!users.Success)
+                return NotFound(users.Error);
+
+            return Ok(users.Value);
+        }
+
+        [HttpGet("{username}")]
+        public async Task<ActionResult<AppUserDto>> GetUser(string username)
+        {
+            var user = await _userService.GetAppUserByUsername(username);
+
+            if (!user.Success)
+                return NotFound(user.Error);
+
+            return Ok(user.Value);
+        }
+    }
+}
