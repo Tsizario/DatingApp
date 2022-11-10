c4b5729390f62a28276285710f05e5bd6494d647
Tsizario <oleksandr.tsisarenko@nure.ua>
2022-11-10 02:14:42 +0200

    Added first errors in Domain

---
+++ src/Dating App/1. Domain/DatingApp.Domain/Constants/Errors.cs
@@ -0,0 +1,9 @@
+﻿namespace DatingApp.Domain.Constants
+{
+    public class Errors
+    {
+        public const string AppUserExists = "The user with such username exists";
+        public const string AppUserNotFound = "The user has not found";
+        public const string AppUsersNotFound = "Users has not found";
+    }
+}
