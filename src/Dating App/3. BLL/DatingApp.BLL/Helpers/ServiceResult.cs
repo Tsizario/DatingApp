7132c47fa409859b3d268d05137ab96379af8b3a
Tsizario <oleksandr.tsisarenko@nure.ua>
2022-11-10 02:10:03 +0200

    Added ServiceResult.cs for convenient handling of mistakes

---
+++ src/Dating App/3. BLL/DatingApp.BLL/Helpers/ServiceResult.cs
@@ -0,0 +1,17 @@
+﻿namespace BLL.Helpers
+{
+    public class ServiceResult<TValue>
+    {
+        public bool Success { get; set; }
+
+        public TValue Value { get; set; }
+
+        public string Error { get; set; }
+
+        public static ServiceResult<TValue> CreateSuccess(TValue value) =>
+            new() { Value = value, Success = true };
+
+        public static ServiceResult<TValue> CreateFailure(string error) =>
+            new() { Error = error, Success = false };
+    }
+}
