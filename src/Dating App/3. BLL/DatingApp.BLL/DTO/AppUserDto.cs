a98abf8ec2e30190f64cf29c7f9f9e4fe364820e
Tsizario <oleksandr.tsisarenko@nure.ua>
2022-11-10 02:07:51 +0200

    Added PhotoDto and AppUserDto

---
+++ src/Dating App/3. BLL/DatingApp.BLL/DTO/AppUserDto.cs
@@ -0,0 +1,35 @@
+﻿using DatingApp.Domain.Entities.Enums;
+
+namespace DatingApp.BLL.DTO
+{
+    public class AppUserDto
+    {
+        public string Username { get; set; }
+
+        public string PhotoUrl { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public GenderType Gender { get; set; }
+
+        public string Bio { get; set; }
+
+        public string LookingFor { get; set; }
+
+        public string Interests { get; set; }
+
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime LastActive { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public ICollection<PhotoDto> Photos { get; set; }
+    }
+}
