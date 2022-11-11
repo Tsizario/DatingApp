58c6fe9c5196fea636505a8f226e51b800b474c0
Tsizario <oleksandr.tsisarenko@nure.ua>
2022-11-11 03:20:46 +0200

    Added new DTO's for login and registration

---
+++ src/Dating App/3. BLL/DatingApp.BLL/DTO/AppUserLoginDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.BLL.DTO
+{
+    public class AppUserLoginDto
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ src/Dating App/3. BLL/DatingApp.BLL/DTO/AppUserRegisterDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.BLL.DTO
+{
+    public class AppUserRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
