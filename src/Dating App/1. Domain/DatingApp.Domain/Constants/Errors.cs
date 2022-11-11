2888114792da37d600bae4aa69b91eee6e523653
Tsizario <oleksandr.tsisarenko@nure.ua>
2022-11-11 03:11:43 +0200

    Added new error to Domain.Constants

--- src/Dating App/1. Domain/DatingApp.Domain/Constants/Errors.cs
+++ src/Dating App/1. Domain/DatingApp.Domain/Constants/Errors.cs
@@ -6,5 +6,7 @@
         public const string AppUserNotFound = "The user has not found";
         public const string AppUsersNotFound = "Users has not found";
         public const string AppUserAddingError = "Error while adding user";
+        public const string AppUserPasswordInvalid = "The password is invalid";
+        public const string AppUserUsernameInvalid = "The username is invalid";
     }
 }
