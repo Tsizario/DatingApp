52a7b5a760f29ce006592102e2fc011a2eabc4c8
Tsizario <oleksandr.tsisarenko@nure.ua>
2022-11-19 23:30:30 +0200

    Fixed register DTO: removed bool property 'Remember me'

--- src/Dating App/3. BLL/DatingApp.BLL/DTO/AppUserRegisterDto.cs
+++ src/Dating App/3. BLL/DatingApp.BLL/DTO/AppUserRegisterDto.cs
@@ -11,7 +11,5 @@ namespace DatingApp.BLL.DTO
         [Required]
         [StringLength(15, MinimumLength = 6)]
         public string Password { get; set; }
-
-        public bool RememberMe { get; set; }
     }
 }
