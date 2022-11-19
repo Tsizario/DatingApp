9f85a0d937f9728a16e45208a054926685df6453
Tsizario <oleksandr.tsisarenko@nure.ua>
2022-11-19 23:28:30 +0200

    Added new requirements for register DTO

--- src/Dating App/3. BLL/DatingApp.BLL/DTO/AppUserRegisterDto.cs
+++ src/Dating App/3. BLL/DatingApp.BLL/DTO/AppUserRegisterDto.cs
@@ -5,9 +5,13 @@ namespace DatingApp.BLL.DTO
     public class AppUserRegisterDto
     {
         [Required]
+        [StringLength(20, MinimumLength = 2)]
         public string Username { get; set; }
 
         [Required]
+        [StringLength(15, MinimumLength = 6)]
         public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
     }
 }
