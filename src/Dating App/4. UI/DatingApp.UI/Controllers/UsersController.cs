a1220482825814f6e50656d5258c0e2507fa04ce
Tsizario <oleksandr.tsisarenko@nure.ua>
2022-11-21 03:05:12 +0200

    Added some advanced English :)

--- src/Dating App/4. UI/DatingApp.UI/Controllers/UsersController.cs
+++ src/Dating App/4. UI/DatingApp.UI/Controllers/UsersController.cs
@@ -1,22 +1,67 @@
 ﻿using DatingApp.BLL.DTO;
+using DatingApp.BLL.Services.TokenService;
 using DatingApp.BLL.Services.UserService;
+using DatingApp.Domain.Constants;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.WebApi.Controllers
 {
-    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
         private readonly IUserService _userService;
+        private readonly ITokenService _tokenService;
 
-        public UsersController(IUserService userService)
+        public UsersController(IUserService userService,
+            ITokenService _tokenService)
         {
             _userService = userService;
+            this._tokenService = _tokenService;
         }
 
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUserTokenDto>> Register(AppUserRegisterDto appUserDto)
+        {
+            var userExists = await _userService.IsAppUserExists(appUserDto.Username);
+
+            if (userExists.Success)
+                return BadRequest(Errors.AppUserExists);
+
+            var result = await _userService.AddAppUser(appUserDto);
+
+            if (!result.Success)
+                return BadRequest(result.Error);
+
+            var appUserTokenDto = new AppUserTokenDto
+            {
+                Username = result.Value.Username,
+                Token = _tokenService.CreateToken(result.Value)
+            };
+
+            return CreatedAtAction(nameof(Register), appUserTokenDto);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUserTokenDto>> Login(AppUserLoginDto loginDto)
+        {
+            var result = await _userService.LoginAppUser(loginDto);
+
+            if (!result.Success)
+                return Unauthorized(result.Error);
+
+            var appUserTokenDto = new AppUserTokenDto
+            {
+                Username = result.Value.Username,
+                Token = _tokenService.CreateToken(result.Value)
+            };
+
+            return Ok(appUserTokenDto);
+        }
+
+
+        [Authorize]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUserDto>>> GetUsers()
         {
@@ -28,6 +73,8 @@ namespace DatingApp.WebApi.Controllers
             return Ok(users.Value);
         }
 
+
+        [Authorize]
         [HttpGet("{username}")]
         public async Task<ActionResult<AppUserDto>> GetUser(string username)
         {
