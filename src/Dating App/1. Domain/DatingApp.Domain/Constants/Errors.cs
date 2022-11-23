3e88dbf79a0c7cddbf3595b0fa9e95024343ce47
Tsizario <oleksandr.tsisarenko@nure.ua>
2022-11-23 02:57:26 +0200

    Corrected some errors

--- src/Dating App/1. Domain/DatingApp.Domain/Constants/Errors.cs
+++ src/Dating App/1. Domain/DatingApp.Domain/Constants/Errors.cs
@@ -3,8 +3,8 @@
     public class Errors
     {
         public const string AppUserExists = "The user with such username exists";
-        public const string AppUserNotFound = "The user has not found";
-        public const string AppUsersNotFound = "Users has not found";
+        public const string AppUserNotFound = "The user has not been found";
+        public const string AppUsersNotFound = "Users has not been found";
         public const string AppUserAddingError = "Error while adding user";
         public const string AppUserPasswordInvalid = "The password is invalid";
         public const string AppUserUsernameInvalid = "The username is invalid";
