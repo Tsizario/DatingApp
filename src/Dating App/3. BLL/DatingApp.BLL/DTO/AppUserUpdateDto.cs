6489028d0d44fbe7ac2f5560a9b2f71f84c0a571
Tsizario <oleksandr.tsisarenko@nure.ua>
2022-11-26 23:25:13 +0200

    Added some js files for tabs in views and added AppUsedDto & AppUserUpdateDto

---
+++ src/Dating App/3. BLL/DatingApp.BLL/DTO/AppUserUpdateDto.cs
@@ -0,0 +1,25 @@
+﻿using DatingApp.Domain.Entities.Enums;
+
+namespace DatingApp.BLL.DTO
+{
+    public class AppUserUpdateDto
+    {
+        public string Name { get; set; }
+
+        public string Bio { get; set; }
+
+        public string LookingFor { get; set; }
+
+        public string Interests { get; set; }
+
+        public string City { get; set; }
+
+        public string Region { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime LastActive { get; set; }
+
+        public ICollection<PhotoDto> Photos { get; set; }
+    }
+}
