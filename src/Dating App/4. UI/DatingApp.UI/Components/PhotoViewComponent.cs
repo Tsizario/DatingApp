3c1756ab52fe7125e79d48809673cd251ca9f228
Tsizario <oleksandr.tsisarenko@nure.ua>
2022-11-26 08:22:25 +0200

    Updated Components

---
+++ src/Dating App/4. UI/DatingApp.UI/Components/PhotoViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using DatingApp.BLL.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.UI.Components
+{
+    public class PhotoViewComponent : ViewComponent
+    {
+        public PhotoViewComponent()
+        {
+        }
+
+        public IViewComponentResult Invoke(PhotoDto photoDto)
+        {
+            return View(photoDto);
+        }
+    }
+}
