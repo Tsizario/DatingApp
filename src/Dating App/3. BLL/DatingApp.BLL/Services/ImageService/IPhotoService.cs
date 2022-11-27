702473c39a535579fb465c330d0da8b87d308c9f
Tsizario <oleksandr.tsisarenko@nure.ua>
2022-11-27 03:12:14 +0200

    Added Cloudinary service for photos

---
+++ src/Dating App/3. BLL/DatingApp.BLL/Services/ImageService/IPhotoService.cs
@@ -0,0 +1,11 @@
+﻿using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+
+namespace DatingApp.BLL.Services.ImageService
+{
+    public interface IPhotoService
+    {
+        Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+        Task<DeletionResult> DeletePhotoAsync(string publicId);
+    }
+}
---
+++ src/Dating App/3. BLL/DatingApp.BLL/Services/ImageService/PhotoService.cs
@@ -0,0 +1,54 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using DatingApp.BLL.Services.CloudinaryService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+
+namespace DatingApp.BLL.Services.ImageService
+{
+    public class PhotoService : IPhotoService
+    {
+        private readonly Cloudinary _cloudinary;
+
+        public PhotoService(IOptions<CloudinarySettings> configuration)
+        {
+            var account = new Account
+            (
+                configuration.Value.CloudName,
+                configuration.Value.ApiKey,
+                configuration.Value.ApiSecret
+            );
+
+            _cloudinary = new Cloudinary(account);
+        }
+        public async Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
+        {
+            var uploadResult = new ImageUploadResult();
+
+            if (file.Length > 0)
+            {
+                using var stream = file.OpenReadStream();
+
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(file.FileName, stream),
+                    Transformation = new Transformation()
+                        .Height(300).Width(300)
+                            .Crop("fill").Gravity("face"),
+                    Folder = "dating-app"
+                };
+
+                uploadResult = await _cloudinary.UploadAsync(uploadParams);
+            }
+
+            return uploadResult;
+        }
+
+        public async Task<DeletionResult> DeletePhotoAsync(string publicId)
+        {
+            var deleteParams = new DeletionParams(publicId);
+
+            return await _cloudinary.DestroyAsync(deleteParams);
+        }
+    }
+}
