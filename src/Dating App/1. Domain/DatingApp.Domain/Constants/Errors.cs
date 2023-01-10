bb09a17800aace7f8157b8e3132b55b68c369e09
Tsizario <oleksandr.tsisarenko@nure.ua>
2023-01-10 20:55:53 +0200

    Added new error for UI

--- src/Dating App/1. Domain/DatingApp.Domain/Constants/Errors.cs
+++ src/Dating App/1. Domain/DatingApp.Domain/Constants/Errors.cs
@@ -4,6 +4,7 @@
     {
         public const string AppUserExists = "The user with such username exists";
         public const string AppUserNotFound = "The user has not been found";
+        public const string AppUserNotUpdated = "The user has not been updated";
         public const string AppUsersNotFound = "Users has not been found";
         public const string AppUserAddingError = "Error while adding user";
         public const string AppUserPasswordInvalid = "The password is invalid";
